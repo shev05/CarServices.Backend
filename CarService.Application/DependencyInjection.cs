7b4eead25a82e4da5afb55dfcc4449839bd45c64
Shevchnko Evgeniy <zhenshev05@mail.ru>
2025-02-13 12:49:12 +0300

    [Added] Dependency injection

---
+++ CarService.Application/DependencyInjection.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CarServices.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(
+            this IServiceCollection services)
+        {
+            services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly));
+            return services;
+        }
+    }
+}
---
+++ CarService.Persistence/DependencyInjection.cs
@@ -0,0 +1,23 @@
+﻿using CarServices.Application.Interface;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CarServices.Persistence
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddPersistence(this IServiceCollection
+            services, IConfiguration configuration)
+        {
+            var connectionString = configuration["DbConnection"];
+            services.AddDbContext<CarServicesDbContext>(options =>
+            {
+                options.UseSqlite(connectionString);
+            });
+            services.AddScoped<ICarServiceDbContext>(provider =>
+            provider.GetService<CarServicesDbContext>());
+            return services;
+        }
+    }
+}
